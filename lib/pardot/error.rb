f6c56a1cdaae5388f1896a7ad8e1f5d62a7f2bf2
Robert Gleeson <rob@flowof.info>
2013-04-27 02:49:57 +0200

    expose the response as ResponseError#code, ResponseError#message
    I've changed ResponseError a little bit to expose the response code & its
    message or content as ruby methods that return the type that makes most sense(so
    for code, that'd be Fixnum, and '__content__' as a string).
    
    I maintained backwards compatibility with the old object in the sense that
    ResponseError#inspect returns the same string as before.
    
    I've found this code to be useful when ResponseError is too generic and I need
    to take certain action based on the response code.

--- lib/pardot/error.rb
+++ lib/pardot/error.rb
@@ -1,8 +1,23 @@
 module Pardot
-  
   class Error < StandardError; end
   class NetError < Error; end
-  class ResponseError < Error; end
   class ExpiredApiKeyError < Error; end
-  
+
+  class ResponseError < Error
+    def initialize(res)
+      @res = res
+    end
+
+    def to_s
+      @res["__content__"]
+    end
+
+    def code
+      @res["code"].to_i
+    end
+
+    def inspect
+      @res.inspect.to_s
+    end
+  end
 end
---
+++ spec/pardot/error_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+describe Pardot::ResponseError do
+  before do
+    @res = {
+      "code" => "9",
+      "__content__" => "A prospect with the specified email address already exists"
+    }
+  end
+
+  describe '#code' do
+    subject do
+      described_class.new(@res).code
+    end
+    specify do
+      should == 9
+    end
+  end
+
+  describe '#to_s, #message' do
+    subject do
+      described_class.new(@res)
+    end
+    specify do
+      subject.to_s.should == @res["__content__"]
+    end
+    specify do
+      subject.message.should == @res["__content__"]
+    end
+  end
+
+  describe '#inspect' do
+    subject do
+      described_class.new(@res).inspect
+    end
+    specify do
+      should == @res.to_s
+    end
+  end
+end
