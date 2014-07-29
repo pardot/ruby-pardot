4faf25bbe9135109150926be93d891194c28103b
Daniel Marvin <dmarvin@capterra.com>
2014-07-28 20:10:31 -0400

    added spec, fixed a couple documentation specifics

--- lib/pardot/objects/prospect_accounts.rb
+++ lib/pardot/objects/prospect_accounts.rb
@@ -13,28 +13,25 @@ module Pardot
 
         def query(search_criteria)
           result = get '/do/query', search_criteria, 'result'
-          result['total_results'] = result['total_results'].to_i if result['total_results']
+          result['total_results'] = result['total_results'].to_i if
+            result['total_results']
           result
         end
 
-        # assign_by_email
-        # assign_by_id
-        # read_by_email
-        # read_by_id
-        # update_by_email
-        # update_by_id
-        # upsert_by_email
-        # upsert_by_id
-        [:assign, :read, :update, :upsert].each do |verb|
-          [:email, :id].each do |direct_to|
-            define_method("#{verb}_by_#{direct_to}") do |directive_value, **params|
-              post(api_url(verb, direct_to, directive_value), params)
-            end
-          end
+        def describe(params={})
+          post('/do/describe', params)
         end
 
-        def create(email, **params)
-          post(api_url(:create, :email, email), params)
+        def create(params={})
+          post('/do/create', params)
+        end
+
+        # read_by_id
+        # update_by_id
+        [:read, :update].each do |verb|
+          define_method(verb) do |id, params={}|
+            post(api_url(verb, 'id', id), params)
+          end
         end
 
         private
@@ -43,13 +40,13 @@ module Pardot
           "/do/#{verb}/#{direct_to}/#{value}"
         end
 
-        def get(path, params={}, result='prospect_account')
-          response = @client.get('prospect_account', path, params)
+        def get(path, params={}, result='prospectAccount')
+          response = @client.get('prospectAccount', path, params)
           result ? response[result] : response
         end
 
-        def post(path, params={}, result='prospect_account')
-          response = @client.post('prospect_account', path, params)
+        def post(path, params={}, result='prospectAccount')
+          response = @client.post('prospectAccount', path, params)
           result ? response[result] : response
         end
       end
---
+++ spec/pardot/objects/prospect_accounts_spec.rb
@@ -0,0 +1,78 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Pardot::Objects::ProspectAccounts do
+
+  before do
+    @client = create_client
+  end
+
+  describe "query" do
+
+    def sample_results
+      %(<?xml version="1.0" encoding="UTF-8"?>
+      <rsp stat="ok" version="1.0">
+        <result>
+          <total_results>2</total_results>
+          <prospectAccount>
+            <name>Spaceships R Us</name>
+          </prospectAccount>
+          <prospectAccount>
+            <name>Monsters Inc</name>
+          </prospectAccount>
+        </result>
+      </rsp>)
+    end
+
+    it "should take in some arguments" do
+      fake_get "/api/prospectAccount/version/3/do/query?assigned=true&format=simple&user_key=bar&api_key=my_api_key", sample_results
+
+      @client.prospect_accounts.query(:assigned => true).should == {'total_results' => 2,
+        'prospectAccount'=>[
+          {'name'=>'Spaceships R Us'},
+          {'name'=>'Monsters Inc'}
+        ]}
+    end
+
+  end
+
+  describe 'read' do
+    def sample_results
+      %(<?xml version="1.0" encoding="UTF-8"?>
+      <rsp stat="ok" version="1.0">
+        <prospectAccount>
+          <id>1234</id>
+          <name>SupaDupaPanda</name>
+        </prospectAccount>
+        </rsp>)
+    end
+
+    it 'should return a valid account' do
+      fake_post '/api/prospectAccount/version/3/do/read/id/1234?assigned=true&format=simple&user_key=bar&api_key=my_api_key', sample_results
+
+      @client.prospect_accounts.read('1234', :assigned => true).should == {'id' => '1234', 'name' => 'SupaDupaPanda' }
+    end
+
+  end
+
+
+  describe 'create' do
+
+    def sample_results
+      %(<?xml version="1.0" encoding="UTF-8"?>
+      <rsp stat="ok" version="1.0">
+        <prospectAccount>
+          <name>SuperPanda</name>
+        </prospectAccount>
+      </rsp>)
+    end
+
+    it 'should return the prospect' do
+      fake_post '/api/prospectAccount/version/3/do/create?api_key=my_api_key&user_key=bar&format=simple&name=SuperPanda', sample_results
+
+      @client.prospect_accounts.create(:name => 'SuperPanda').should == {"name"=>"SuperPanda"}
+
+    end
+
+  end
+
+end
