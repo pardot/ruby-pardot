3b7e513499f56a3d0ad99889e3a3bc614fa1f7bf
polygonplanet <polygon.planet.aqua@gmail.com>
2019-05-22 19:12:43 +0900

    Add create method

--- lib/pardot/objects/list_memberships.rb
+++ lib/pardot/objects/list_memberships.rb
@@ -1,3 +1,5 @@
+# Modified to create a new list membership as "create" under the MIT license.
+
 module Pardot
   module Objects
     module ListMemberships
@@ -22,10 +24,20 @@ module Pardot
           get "/do/read/id/#{id}", params
         end
 
+        # Add create
+        def create list_id, prospect_id, params = {}
+          post "/do/create/list_id/#{list_id}/prospect_id/#{prospect_id}", params
+        end
+
         protected
 
         def get path, params = {}, result = "listMembership"
           response = @client.get "listMembership", path, params
+          result ? response[result] : response
+        end
+
+        def post path, params = {}, result = "listMembership"
+          response = @client.post "listMembership", path, params
           result ? response[result] : response
         end
 
