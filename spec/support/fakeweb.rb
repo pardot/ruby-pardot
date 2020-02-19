e9db9e9ccf9e850e6b2c7a8ec3cc35c80663839b
Eric Binmore <eric.binmore@voxmedia.com>
2020-02-19 11:10:08 -0500

    Use string keys instead of symbol keys

--- spec/support/fakeweb.rb
+++ spec/support/fakeweb.rb
@@ -1,18 +1,18 @@
 require 'fakeweb'
 FakeWeb.allow_net_connect = false
 
-def fake_post path, response
-  FakeWeb.register_uri(:post, "https://pi.pardot.com#{path}", :body => response)
+def fake_post(path, response)
+  FakeWeb.register_uri(:post, "https://pi.pardot.com#{path}", body: response)
 end
 
-def fake_get path, response
-  FakeWeb.register_uri(:get, "https://pi.pardot.com#{path}", :body => response)
+def fake_get(path, response)
+  FakeWeb.register_uri(:get, "https://pi.pardot.com#{path}", body: response)
 end
 
-def fake_authenticate client, api_key
+def fake_authenticate(client, api_key)
   client.api_key = api_key
 end
 
 def assert_authorization_header
-  expect(FakeWeb.last_request[:authorization]).to eq('Pardot api_key=my_api_key, user_key=bar')
+  expect(FakeWeb.last_request['authorization']).to eq('Pardot api_key=my_api_key, user_key=bar')
 end
